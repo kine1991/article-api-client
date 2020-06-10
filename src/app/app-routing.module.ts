c9aeb79ad9098f8103ad3f68e4957af551619aea
Николай Киселев <kine4599@gmail.com>
2020-06-10 13:25:50 +0300

    add responsive 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ArticleComponent } from './article/article.component';
-import { SignInComponent } from './sign-in/sign-in.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-
 
 const routes: Routes = [
   // {
   //   path: '',
   //   component: ArticleComponent
-  // },
-  // {
-  //   path: 'articles',
-  //   component: ArticleComponent
-  // },
-  // {
-  //   path: 'sign-in',
-  //   component: SignInComponent
-  // },
-  // {
-  //   path: 'sign-up',
-  //   component: SignUpComponent
   // },
   {
     path: '',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,11 @@ import { AppComponent } from './app.component';
 import { ArticleComponent } from './article/article.component';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SignInComponent } from './sign-in/sign-in.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ArticleComponent,
-    SignInComponent,
-    SignUpComponent
   ],
   imports: [
     BrowserModule,
